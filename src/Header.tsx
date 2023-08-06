1ed29970ac3d27c832ff225b87d2547ee5e3184f
Sentaro <ui-10@outlook.jp>
2023-08-06 11:19:08 +0900

    Header調整

---
+++ src/Header.tsx
@@ -0,0 +1,20 @@
+
+
+function Header() {
+
+  return (
+    <>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <nav className="mx-auto list-none">
+        <li><a href="#">Top</a></li>
+        <li><a href="#">Works</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </nav>
+    </>
+  )
+}
+
+export default Header
