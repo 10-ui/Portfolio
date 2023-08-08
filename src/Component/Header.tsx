d7f51ff2a9a791719999b3e79147f190053ef2d4
Sentaro <ui-10@outlook.jp>
2023-08-08 18:20:30 +0900

    ?

--- src/Component/Header.tsx
+++ src/Component/Header.tsx
@@ -9,7 +9,7 @@ export const Header:React.FC = ()=> {
       <header id="header" className="fixed left-0 top-0 grid w-1/5 h-screen place-items-center mr-auto bg-header">
         <nav className="mx-auto list-none my-5">
           <div className="w-36">
-            <img src="./src/assets/kuma.png" alt="かわいい熊" />
+            <img src="../assets/kuma.png" alt="かわいい熊" />
           </div>
           <ul className="text-center my-20 font-gnav">
             <li className="my-5 text-3xl"><Link to="/">Top</Link></li>
